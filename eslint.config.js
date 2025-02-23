1e7647b41135fd4bd2cd2dfd71deeb8b41b818e2
dmardoqueu <diogomardoqueu@gmail.com>
2025-02-23 17:57:12 -0300

    Criando páginas e rotas

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+export default [
+    {
+      rules: {
+        // Exemplo de regras
+        "no-unused-vars": "error",
+        "semi": ["error", "always"],
+      },
+    },
+  ];
